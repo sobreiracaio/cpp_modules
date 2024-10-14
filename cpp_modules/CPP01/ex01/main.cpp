0ba3214037958c816d581801184a59054e38629b
sobreiracaio <caiosobreira@hotmail.com>
2024-10-14 21:53:37 +0100

    cpp00

--- cpp_modules/CPP01/ex01/main.cpp
+++ cpp_modules/CPP01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: crocha-s <crocha-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:38:38 by crocha-s          #+#    #+#             */
-/*   Updated: 2024/10/14 17:59:26 by crocha-s         ###   ########.fr       */
+/*   Updated: 2024/10/14 21:27:54 by crocha-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 int main(void)
 {
+	int i = 0;
 	int size;
 	Zombie *zombie;
 	std::string name;
 
-	std::cin.ignore()
-	
+	std::cout << "Enter horde size: " << std::endl;
+	std::cin >> size;
+	std::cout << "Enter zombie names: " << std::endl;
+	std::cin >> name;
+	zombie = zombieHorde(size, name);
+	while (i < size)
+	{
+		zombie[i].announce();
+		i++;
+	}
+	delete [] zombie;
 }
