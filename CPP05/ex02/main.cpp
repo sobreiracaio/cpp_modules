7532dec31c9022336d391f6ab0539a0e9d005267
Caio Rocha Sobreira <crocha-s@c1r16s5.42porto.com>
2025-01-07 18:41:46 +0000

    forms finished need main

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -23,54 +23,6 @@ void proceed(std::string msg)
 int main(void)
 {
     proceed("Test 1 - Bureaucrat not skilled enough to sign a form.");
-    Bureaucrat *parameter = NULL;
-    try
-    {
-        parameter = new Bureaucrat("John", 120);
-    }
-    catch(Bureaucrat::GradeTooHighException &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    catch(Bureaucrat::GradeTooLowException &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << parameter << std::endl;
-       
-    Form *form = new Form("Parameter Form", 119, 118);
-
-    try
-    {
-        parameter->signForm(*form);
-    }
-    catch(Form::GradeTooLowException &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    std::cout << form << std::endl;
-    
-    proceed("Test 2 - Grade will be incremented");
-    parameter->incrementGrade();
-    std::cout << parameter << std::endl;
-
-    try
-    {
-        parameter->signForm(*form);
-    }
-    catch(Form::GradeTooHighException &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    catch(Form::GradeTooLowException &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    
-    std::cout << form << std::endl;
-    
 
     
     
