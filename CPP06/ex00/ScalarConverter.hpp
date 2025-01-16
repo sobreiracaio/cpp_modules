e827290d8599d74f825dff4d7b2cbdf3218b4f71
sobreiracaio <caiosobreira@hotmail.com>
2025-01-16 20:12:30 +0000

    cpp06

---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crocha-s <crocha-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 19:57:14 by crocha-s          #+#    #+#             */
+/*   Updated: 2025/01/16 19:59:42 by crocha-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class ScalarConverter {
+    private:
+
+    public:
+    
+        void convert(std::string data);
+}
