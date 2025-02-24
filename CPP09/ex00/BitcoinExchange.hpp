5af42e4eac3ece604c1440b367d135e2d920109d
sobreiracaio <caiosobreira@hotmail.com>
2025-02-24 23:43:26 +0000

    ex00

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: crocha-s <crocha-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 16:53:52 by crocha-s          #+#    #+#             */
-/*   Updated: 2025/02/24 18:27:49 by crocha-s         ###   ########.fr       */
+/*   Updated: 2025/02/24 23:42:31 by crocha-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,25 @@
 #define CLEAR   "\033c"
 
 
+typedef struct data
+        {
+            std::string date;
+            float price;
+        }Data;
 class BitcoinExchange {
     
     private:
         std::string _fileName;
-        std::string _rawData;
-        std::vector<std::string> _parsedData;
+        
+        
+        std::vector<data> _parsedData;
     public:
         BitcoinExchange(std::string file);
         //BitcoinExchange(const BitcoinExchange &copy);
         ~BitcoinExchange();
         //BitcoinExchange &operator=(BitcoinExchange const &source);
         
-        void readFromFile();
+        std::vector<std::string> readFromFile(Data &data);
         //std::vector<std::string> parseAndPopulate();
     
 };
